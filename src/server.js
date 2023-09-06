5c798c1ea3306b612d3c8325fb8ec668fa3b0c78
Edna <edna.maria887@gmail.com>
2023-09-06 10:24:31 -0300

    feat: conexão com o banco de dados

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 // imports
 require("express-async-errors")
-
 const AppError = require("./utils/AppError")
+
+const database = require("./database/sqlite/index");
+
 const express = require("express");
 const routes = require("./routes")
 
@@ -10,6 +12,8 @@ const routes = require("./routes")
 const app = express();
 app.use(express.json());
 app.use(routes);
+
+database(); //executando o banco de dados
 
 app.use((error,request,response, next) => {
 
