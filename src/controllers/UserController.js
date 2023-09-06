7a9b4ba3b1cb97a6e4522f05c7e72f78e2025eb5
Edna <edna.maria887@gmail.com>
2023-09-06 10:02:31 -0300

    feat: padronização do tratamento de erros

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,13 +1,20 @@
 //parte lógica para as requisições e respostas
+const AppError = require("../utils/AppError");
 
 class UserController {
 
   create(req, res) {
     const {name, email, password} = req.body;
 
-    res.json({name, email, password});
+    //validando o campo de name
+    if(!name){
+      throw new AppError("Insira o seu nome!");
+    }
+
+    res.status(201).json({name, email, password});
   };
 };
 
 
 module.exports = UserController;
+
---
+++ src/utils/AppError.js
@@ -0,0 +1,12 @@
+class AppError extends Error{
+  message;
+  statusCode;
+
+  constructor(message, statusCode = 400) {
+    super()
+    this.message = message;
+    this.statusCode = statusCode;
+  }
+}
+
+module.exports = AppError;
