9b984b486d7d476db36c6646249b433808fc1741
Edna <edna.maria887@gmail.com>
2023-09-10 22:52:53 -0300

    feat: criado a estrutura para receber imagens e alterar(caso já exista) pela rota de update

---
+++ src/controllers/DishImageController.js
@@ -0,0 +1,30 @@
+//imports
+const knex = require("../database/knex");
+
+const DiskStorage = require("../providers/DiskStorage");
+
+class DishImageController {
+  async update (req, res){
+    const {id} = req.params;
+
+  const diskStorage = new DiskStorage();
+
+  const dishFilename = req.file.filename;
+
+  const dish = await knex("dishes").where({id}).first();
+
+  if(dish.image){
+    await diskStorage.deleteFile(dish.image);
+  }
+
+  const filename = await diskStorage.saveFile(dishFilename);
+  dish.image = filename;
+
+  await knex("dishes").update(dish).where({id});
+
+  return res.json(dish);
+  }
+
+}
+
+module.exports = DishImageController;
--- src/routes/dishesAdmin.routes.js
+++ src/routes/dishesAdmin.routes.js
@@ -1,14 +1,22 @@
 //imports
 const {Router} = require("express");
 
+//Multer para o carregemento de imagens e configs
+const multer = require("multer");
+const uploadConfig = require("../configs/upload");
+
 const DishesAdminController = require("../controllers/DishesAdminController");
+const DishImageController = require("../controllers/DishImageController");
 
 const dishesAdminRoutes = Router();
+const upload = multer(uploadConfig.MULTER);
 
 const dishesAdminController = new DishesAdminController();
+const dishImageController = new DishImageController();
 
 dishesAdminRoutes.post('/', dishesAdminController.create);
 dishesAdminRoutes.delete('/:id', dishesAdminController.delete);
 dishesAdminRoutes.put('/:id', dishesAdminController.update);
+dishesAdminRoutes.patch('/dishImage/:id', upload.single("image"), dishImageController.update);
 
 module.exports = dishesAdminRoutes;
